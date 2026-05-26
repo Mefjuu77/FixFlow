1a0c9fe50efacc56faab8e4852b0cda60594d16f
Mefjuu77 <mateuszstruszczyk@gmail.com>
2026-05-26 20:06:36 +0200

    poprawki

--- config/settings.py
+++ config/settings.py
@@ -42,12 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # Third-party apps
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     
-    # Local apps
     'accounts',
     'tickets',
 ]
