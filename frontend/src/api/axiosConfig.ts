47ff59e5530ff240e53bf6f77de5bcd515b9a9e4
Mefjuu77 <mateuszstruszczyk@gmail.com>
2026-05-26 22:16:06 +0200

    Update axiosConfig.ts

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -27,7 +27,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/',
 });
 
-// Interceptor dołączający token JWT do każdego zapytania
 api.interceptors.request.use(
   (config) => {
     const token = getAccessToken();
